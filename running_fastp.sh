7bdd051dea074a1aeac18409c0ddf31a2695a064
aaryanchhabra <aryanchhabra28@gmail.com>
2022-06-17 19:27:44 +0530

    update

--- running_fastp.sh
+++ running_fastp.sh
@@ -1,3 +1,5 @@
+#make sure you have all the raw reads in ../raw_reads
+
 mkdir bresso_data/trimmed_raw_reads
 
 cat SRR_Acc_List.txt | parallel -j 4 "fastp -i raw_reads/{}.fq.gz -o trimmed_raw_reads/{}.fq.gz"
