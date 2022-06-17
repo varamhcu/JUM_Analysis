39f5d32cb1f015adee945966ea53c0fdce643f9e
aaryanchhabra <aryanchhabra28@gmail.com>
2022-06-18 01:45:57 +0530

    Update STAR_indexing.sh

--- STAR_indexing.sh
+++ STAR_indexing.sh
@@ -2,16 +2,21 @@
 
 cd bresso_data
 
-#indexing from TAIR10.1 from new source
+#indexing from TAIR10.1 with new --sjdboverhang value
+mkdir STAR_index_10.1_N
 
 STAR --runThreadN 8 \
 --runMode genomeGenerate \
---genomeDir STAR_index_10.1_N \ # to run genome indices generation job
---genomeFastaFiles reference_files/GCF_000001735.4_TAIR10.1_genomic.fna \ #the path to genome reference file
---sjdbGTFfile reference_files/GCF_000001735.4_TAIR10.1_genomic.gtf \ #the path to annotation file
---sjdbOverhang 96 # the length of the genomic sequence around the annotated junction to be used in constructing the splice junctions database. Ideally, this length should be equal to the lenght of the read-1.
+--genomeSAindexNbases 12 \
+--genomeDir STAR_index_10.1_N \ 
+--genomeFastaFiles reference_files/GCF_000001735.4_TAIR10.1_genomic.fna \
+--sjdbGTFfile reference_files/GCF_000001735.4_TAIR10.1_genomic.gtf \
+--sjdbOverhang 96
+
+# the above alue is the length of the genomic sequence around the annotated junction to be used in constructing the splice junctions database. Ideally, this length should be equal to the lenght of the read-1.
 
 #As genePred file formation was failed with TAIR10.1 ; A new indexing with TAIR10.27 (?)
+mkdir STAR_index_10.27
 
 STAR --runThreadN 8 \
 --runMode genomeGenerate \
